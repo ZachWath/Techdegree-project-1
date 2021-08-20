03b17ec594f3f3c90d1b309e8ca11c97bfed5042
ZachWath <89270314+ZachWath@users.noreply.github.com>
2021-08-20 16:30:09 -0500

    main project , b4 extra c
    final base project , before extra credit

--- Techdegree-1/a_random_quote_generator-v1/js/script.js
+++ Techdegree-1/a_random_quote_generator-v1/js/script.js
@@ -3,34 +3,82 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * In this array, named quotes, I have stored all of the quotes that are to be randomly generated along with any pertinant information. 
 ***/
 const quotes = [
-{},
-{},
-{},
-{},
-{},
-{}
+{
+  quote: 'Are you suggesting coconuts migrate?',
+  source: 'Soldier',
+  citation: 'Monty Python and the Holy Grail.',
+  year: '1975'
+},
+{
+  quote: 'Just a flesh wound.',
+  source: 'The Black Knight',
+  citation: 'Monty Python and the Holy Grail.',
+  year: '1975'
+},
+{
+  quote: 'Never tell me the odds!',
+  source: 'Han Solo',
+  citation: 'Star Wars: The Empire Strikes Back',
+  year: '1980'
+},
+{
+  quote: 'I’m just a simple man trying to make my way in the universe.',
+  source: 'Jango Fett',
+  citation: 'Star Wars: Attack of the Clones',
+  year: '2002'
+},
+{
+  quote: 'Deeds will not be less valiant because they are unpraised.',
+  source: 'Aragorn',
+  citation: 'The Lord of the Rings: The Return of the King.',
+  year: '2003'
+},
+{
+  quote: 'Even darkness must pass. A new day will come. And when the sun shines it will shine out the clearer.',
+  source: 'Samwise Gamgee',
+  citation: 'The Lord of the Rings: The Two Towers.',
+  year: '2002'
+}
 
 ]
 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function = this funciton serves to return a random quote from the quotes array to be displayed on the page.
 ***/
 
-
+function getRandomQuote () {
+  let q = Math.floor(Math.random()*(quotes.length));
+  return quotes [q];
+}
 
 /***
- * `printQuote` function
+ * `printQuote` function = this function serves to print the randomly generated quote to the webpage in a uniform and orderly manner. 
 ***/
-
+function printQuote () {
+ let randomQuote = getRandomQuote ();
+ let html = `
+  <p class="quote"> ${randomQuote.quote} </p>
+  <p class="source"> ${randomQuote.source} 
+ `;
+ if (randomQuote.citation){
+    html += `
+      <span class="citation"> ${randomQuote.citation} </span>
+    `
+ }
+if(randomQuote.year){
+   html += `
+    <span class="year"> ${randomQuote.year} </span>
+  `
+}
+ html += `</p>`;
+document.getElementById('quote-box').innerHTML = html;
+}
 
 
 /***
